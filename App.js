ba44ba3b1d462d03000209b6744c6df7f302688b
Sabarinath <sabarinath3072001@gmail.com>
2021-08-29 16:33:25 +0530

    'refresh when offline' feature added

--- App.js
+++ App.js
@@ -13,16 +13,17 @@ import {
 import Thought from "./components/Thought";
 import { db } from "./firebase";
 import firebase from "firebase/app";
+import NetInfo from "@react-native-community/netinfo";
 
 export default function App() {
   const [data, setData] = useState("");
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(undefined);
+  const [isOnline, setIsOnline] = useState(false);
   const [refresh, setRefresh] = useState(false);
 
-  useEffect(async () => {
-    async function fetchData() {
-      await db
-        .collection("thoughts")
+  useEffect(() => {
+    function fetchData() {
+      db.collection("thoughts")
         .orderBy("createdAt", "desc")
         .get()
         .then((snapshot) => {
@@ -31,24 +32,36 @@ export default function App() {
           setItems(list);
         });
     }
+    NetInfo.fetch().then((state) => {
+      const status = state.isConnected && state.isInternetReachable;
+      setIsOnline(status);
+    });
     fetchData();
   }, [refresh]);
 
   const handleAddThought = async () => {
-    Keyboard.dismiss();
-    if (data != "") {
-      await db.collection("thoughts").add({
-        thought: data,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      });
-      setRefresh(!refresh);
-      setData("");
+    try {
+      Keyboard.dismiss();
+      if (data != "") {
+        await db.collection("thoughts").add({
+          thought: data,
+          createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        setRefresh(!refresh);
+        setData("");
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
   const handleDeleteThought = async (id) => {
-    await db.collection("thoughts").doc(id).delete();
-    setRefresh(!refresh);
+    try {
+      await db.collection("thoughts").doc(id).delete();
+      setRefresh(!refresh);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
@@ -72,18 +85,37 @@ export default function App() {
             </View>
           </TouchableOpacity>
         </KeyboardAvoidingView>
-        <View style={styles.tasksWrapper}>
-          <View style={styles.items}>
-            {items.map((item) => (
-              <TouchableOpacity
-                key={item.id}
-                onPress={() => handleDeleteThought(item.id)}
-              >
-                <Thought text={item.thought} />
-              </TouchableOpacity>
-            ))}
+        {isOnline ? (
+          items ? (
+            <View style={styles.tasksWrapper}>
+              <View style={styles.items}>
+                {items.map((item) => (
+                  <TouchableOpacity
+                    key={item.id}
+                    onPress={() => handleDeleteThought(item.id)}
+                  >
+                    <Thought text={item.thought} />
+                  </TouchableOpacity>
+                ))}
+              </View>
+              <View />
+            </View>
+          ) : (
+            <Text style={styles.loadingText}>Loading...</Text>
+          )
+        ) : (
+          <View>
+            <Text style={styles.offlineText}>Please go online...</Text>
+            <TouchableOpacity
+              style={styles.buttonParent}
+              onPress={() => setRefresh(!refresh)}
+            >
+              <View style={styles.refreshButton}>
+                <Text style={styles.refreshButtonText}>Refresh</Text>
+              </View>
+            </TouchableOpacity>
           </View>
-        </View>
+        )}
       </ScrollView>
     </View>
   );
@@ -134,5 +166,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
     borderColor: "#C0C0C0",
     borderWidth: 1,
+  },
+  loadingText: {
+    textAlign: "center",
+    marginTop: 50,
+    fontSize: 20,
+  },
+  offlineText: {
+    marginTop: 50,
+    textAlign: "center",
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  refreshButton: {
+    backgroundColor: "aquamarine",
+    width: 100,
+    height: 30,
+    justifyContent: "center",
+    alignSelf: "center",
+    borderRadius: 20,
+  },
+  refreshButtonText: {
+    textAlign: "center",
   },
 });
